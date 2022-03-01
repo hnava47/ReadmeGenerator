195738c80552f4ab3e83d2727642ea1edb1afa35
Hector Nava <hector.r.nava44@gmail.com>
2022-03-01 12:19:58 -0700

    Added README logic

--- index.js
+++ index.js
@@ -1,13 +1,62 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const markdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'Provide the title of the project:',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Provide description of the project:',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'Provide installation instructions of the project:',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'Provide usage information of the project:',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'Provide contribution guidelines of the project:',
+        name: 'contributing'
+    },
+    {
+        type: 'input',
+        message: 'Provide test instructions of the project',
+        name: 'tests'
+    },
+    {
+        type: 'list',
+        message: 'Select license type:',
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+        name: 'license'
+    },
+    {
+        type: 'input',
+        message: 'Provide GitHub username:',
+        name: 'githubUsername'
+    },
+    {
+        type: 'input',
+        message: 'Provide email:',
+        name: 'email'
+    }
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const init = () => {
+    inquirer.prompt(questions).then((response) => {
+        fs.writeFile('README.md', markdown(response), (err) =>
+            err ? console.error(err) : console.log('Readme successfully created!')
+        );
+    });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,85 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const renderLicenseBadge = (license) => {
+    if (!license) {
+        return '';
+    }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+    if (license === 'MIT License') {
+        return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    } else if (license === 'GNU AGPLv3') {
+        return '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)';
+    } else if (license === 'GNU GPLv3') {
+        return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+    } else if (license == 'GNU LGPLv3') {
+        return `[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)`;
+    } else if (license === 'Mozilla Public License 2.0') {
+        return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
+    } else if (license === 'Apache License 2.0') {
+        return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+    } else if (license === 'Boost Software License 1.0') {
+        return `[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`;
+    } else if (license === 'The Unlicense') {
+        return `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`;
+    }
+};
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+const generateMarkdown = (data) => {
+    const licenseBadge = renderLicenseBadge(data.license);
+    return `<div id="top"></div>
+<br/>
+<div align="center">
+    <h1 align="center">${data.title}</h1>
+</div>
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+<details>
+    <summary>Table of Contents</summary>
+    <ol>
+        <li><a href="#description">Description</a></li>
+        <li><a href="#installation">Installation</a></li>
+        <li><a href="#usage">Usage</a></li>
+        <li><a href="#contributing">Contributing</a></li>
+        <li><a href="#tests">Tests</a></li>
+        <li><a href="#tests">License</a></li>
+        <li><a href="#tests">Questions</a></li>
+    </ol>
+</details>
+<br/>
 
-`;
-}
+## Description
+${data.description}
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+## Installation
+${data.installation}
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+## Usage
+${data.usage}
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+## Contributing
+${data.contributing}
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+## Tests
+${data.tests}
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+## License
+${licenseBadge}
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+## Questions
+GitHub: [${data.githubUsername}](https://github.com/${data.githubUsername})
+
+Email: [${data.email}](${data.email})
+
+<p align="right">(<a href="#top">back to top</a>)</p>`
+};
 
 module.exports = generateMarkdown;
