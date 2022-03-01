9a360b0c5e991d78d0cd496a0d9bc403bce08575
Hector Nava <hector.r.nava44@gmail.com>
2022-03-01 12:25:35 -0700

    reorganized the list of licenses

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const questions = [
     {
         type: 'list',
         message: 'Select license type:',
-        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+        choices: ['MIT License', 'GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'Boost Software License 1.0', 'The Unlicense'],
         name: 'license'
     },
     {
